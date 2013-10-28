9b6d886fa1dd41393e287f0d7fcc32bbbaf2e052
Clasker <clasker1@gmail.com>
2013-10-28 20:22:32 +0300

    add templates

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.set('views', './views');
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/views', express.static(__dirname + '/views'));
 
+app.use(express.json());
+app.use(express.urlencoded());
 
 
 app.use(app.router);
@@ -16,7 +19,30 @@ app.get('/', function (req, res) {
     res.render('index2');
 });
 
-app.get('/enter', function(req, res){
+
+app.get('/enter', function (req, res) {
+    console.log(req.params);
     res.render('index');
 });
+
+app.post('/showUserDialog', function (req, res) {
+    var userName = 'Not found';
+    var id = 1*req.body.userId;
+// Получение инфы  о юзере
+    switch (id) {
+        case 1: userName = 'Meepo';
+            break;
+        case 2: userName = 'Timbersaw';
+            break;
+        case 3: userName = 'Antimage';
+            break;
+        case 4: userName = 'Batrider';
+            break;
+        case 5: userName = 'Invoker';
+            break;
+    }
+    res.send({"userName": userName, "status" : 'online', 'image' : 'def_user'});
+    res.end();
+});
+
 app.listen(8080);
