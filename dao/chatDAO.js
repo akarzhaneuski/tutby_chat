5f5230906148e3528562897e760b9253df344e77
Clasker <clasker1@gmail.com>
2013-12-02 17:57:54 +0300

    premerge commit

---
+++ dao/chatDAO.js
@@ -0,0 +1,33 @@
+var chatMessages = [];
+
+chatMessages.push({
+    user: 'Petya',
+    message: 'Hi, VASYA!',
+    chanel: 1
+});
+
+exports.addMessage = function (mess, chanel, user) {
+    console.log("chatDAO: add message to storage" );
+    chatMessages.push({
+        user: user,
+        message: mess,
+        chanel: chanel
+    });
+    //console.log(chatMessages);
+}
+exports.getLength = function () {
+    console.log("chatDAO: get current length");
+    return chatMessages.length;
+}
+
+exports.getMessagesByChanel = function (chanel){
+    console.log("chatDAO: get messages by chanel");
+    var ret = [];
+    var length = chatMessages.length;
+    for(var i = 0; i < length; ++i){
+        if(chatMessages[i].chanel == chanel){
+            ret.push(chatMessages[i]);
+        }
+    }
+    return ret;
+}
---
+++ dao/userChatDAO.js
@@ -0,0 +1,33 @@
+var users = [];
+
+
+function isExistsListener(chanel, user) {
+    var length = users.length;
+    for (var i = 0; i < length; ++i) {
+        if (users[i].chanel == chanel && users[i].user == user) {
+            return true;
+        }
+    }
+    return false;
+}
+
+exports.addNewListener = function (chanel, user, connection) {
+    if (!isExistsListener(chanel, user)) {
+        users.push({
+            chanel: chanel,
+            user: user,
+            connection: connection
+        });
+        //console.log(users);
+    }
+}
+
+exports.getAllUsersByChanel = function (chanel){
+    var ret = [];
+    var length = users.length;
+    for(var i = 0; i < length; ++i){
+        if(users[i].chanel == chanel){}
+        ret.push(users[i].connection);
+    }
+    return ret;
+}
