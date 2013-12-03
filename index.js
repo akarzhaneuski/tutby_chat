12b7f53ad504d8051c39a339f85507c1f459eac2
Qulon <mikhailmuchko@gmail.com>
2013-12-03 15:44:00 +0300

    Просто коммит

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(express.logger("dev"));
 app.use(express.cookieParser());
-app.use(express.session({secret : 'andruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuusha'}));
+app.use(express.session({secret: 'andruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuusha'}));
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(app.router);
@@ -23,11 +23,11 @@ var connection = mysql.createConnection({
     password: "hello",
     database: "tutby_chat"
 });
-connection.connect(function(err) {
-    if(err != null) {
+connection.connect(function (err) {
+    if (err != null) {
         console.log("Error connecting to mysql");
     }
-    else{
+    else {
         console.log("Connected to mysql");
     }
     // connected! (unless `err` is set)
@@ -43,66 +43,66 @@ function checkAuth(req, res, next) {
 app.get('/', function (req, res) {
     res.redirect('/main');
 });
-app.get('/main',checkAuth, function (req, res) {
+app.get('/main', checkAuth, function (req, res) {
     res.render('main');
 });
 app.get('/login', function (req, res) {
     res.render('login');
 });
-app.get('/registration',function (req, res) {
+app.get('/registration', function (req, res) {
     res.render('registration');
 });
 app.post('/login', function (req, res) {
     var user = req.body.user;
-    var password =req.body.password;
-    var hash_password=crypto.createHash('sha1').update(password).digest('hex');;
+    var password = req.body.password;
+    var hash_password = crypto.createHash('sha1').update(password).digest('hex');
+    ;
     var sql = "SELECT password,id FROM users WHERE login = ?";
-    connection.query(sql,[user], function(err, results) {
-        if( results[0] == undefined ){
-            console.log("Login failure for %s!",user);
+    connection.query(sql, [user], function (err, results) {
+        if (results[0] == undefined) {
+            console.log("Login failure for %s!", user);
             res.redirect('/login');
-        }else{
-            if(results[0].password == hash_password)
-            {
-                req.session.user_id=results[0].id;
-                console.log("Login success for %s!",user);
+        } else {
+            if (results[0].password == hash_password) {
+                req.session.user_id = results[0].id;
+                console.log("Login success for %s!", user);
                 res.redirect('/main');
-            }else{
-                console.log("Login failure for %s!",user);
+            } else {
+                console.log("Login failure for %s!", user);
                 res.redirect('/login');
             }
         }
     });
 });
-app.post('/registration',function(req, res){
+app.post('/registration', function (req, res) {
     var user = req.body.user;
-    var password =req.body.password;
-    var hash_password=crypto.createHash('sha1').update(password).digest('hex');
+    var password = req.body.password;
+    var hash_password = crypto.createHash('sha1').update(password).digest('hex');
     var sql = "SELECT login FROM users WHERE login = ? ";
-    connection.query(sql,[user],function(err, results) {
-        if(results[0] == undefined){
+    connection.query(sql, [user], function (err, results) {
+        if (results[0] == undefined) {
             var sql = "INSERT INTO users(login,password)  VALUES( ?,? )";
-            connection.query(sql,[user,hash_password],function(err, results){
+            connection.query(sql, [user, hash_password], function (err, results) {
             });
-            console.log('Registration success for %s!',user);
+            console.log('Registration success for %s!', user);
             res.redirect('/login');
-        }else{
-            console.log('Registration failure for %s!',user);
+        } else {
+            console.log('Registration failure for %s!', user);
             res.redirect('/registration');
         }
     });
 
 });
-app.get('/logout',checkAuth, function (req,res){
-    req.session.user_id=undefined;
+app.get('/logout', checkAuth, function (req, res) {
+    req.session.user_id = undefined;
     res.redirect('/login');
 });
 
-app.get('/set_offline',checkAuth, function (req, res) {
+app.get('/set_offline', checkAuth, function (req, res) {
     var user_id = req.session.user_id;
-    connection.query("update users set status = 0 where id= ? ;",[user_id], function(err, rows){
+    connection.query("update users set status = 0 where id= ? ;", [user_id], function (err, rows) {
         // There was a error or not?
-        if(err != null) {
+        if (err != null) {
             res.end("Query error:" + err);
             connection.end();
         } else {
@@ -113,10 +113,10 @@ app.get('/set_offline',checkAuth, function (req, res) {
     console.log('now im offline');
     res.redirect('back');
 });
-app.get('/set_online',checkAuth, function (req, res) {
+app.get('/set_online', checkAuth, function (req, res) {
     var user_id = req.session.user_id;
-    connection.query("update users set status=1 where id= ? ;",[user_id], function(err, rows){
-        if(err != null) {
+    connection.query("update users set status=1 where id= ? ;", [user_id], function (err, rows) {
+        if (err != null) {
             res.end("Query error:" + err);
             connection.end();
         } else {
@@ -127,11 +127,11 @@ app.get('/set_online',checkAuth, function (req, res) {
     console.log('now im online');
     res.redirect('back');
 });
-app.get('/set_out',checkAuth, function (req, res) {
+app.get('/set_out', checkAuth, function (req, res) {
     var user_id = req.session.user_id;
-    connection.query("update users set status=2 where id= ? ;",[user_id],function(err, rows){
+    connection.query("update users set status=2 where id= ? ;", [user_id], function (err, rows) {
         // There was a error or not?
-        if(err != null) {
+        if (err != null) {
             res.end("Query error:" + err);
             connection.end();
         } else {
@@ -142,11 +142,11 @@ app.get('/set_out',checkAuth, function (req, res) {
     console.log('now im out');
     res.redirect('back');
 });
-app.get('/set_busy',checkAuth, function (req, res) {
+app.get('/set_busy', checkAuth, function (req, res) {
     var user_id = req.session.user_id;
-    connection.query("update users set status=3 where id= ? ;",[user_id], function(err, rows){
+    connection.query("update users set status=3 where id= ? ;", [user_id], function (err, rows) {
         // There was a error or not?
-        if(err != null) {
+        if (err != null) {
             res.end("Query error:" + err);
             connection.end();
         } else {
@@ -158,7 +158,7 @@ app.get('/set_busy',checkAuth, function (req, res) {
     res.redirect('back');
 });
 
-app.post('/showUserDialog',checkAuth, function (req, res) {
+app.post('/showUserDialog', checkAuth, function (req, res) {
     var userName = 'Not found';
     var id = +req.body.userId;
 // Получение инфы  о юзере
